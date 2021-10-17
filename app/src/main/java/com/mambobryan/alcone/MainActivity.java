8e3089b4154169ddb91e6fd19166e4b2f34afa6f
Fany S <fhitrisahe@gmail.com>
2021-10-17 23:29:20 +0700

    first commit

---
+++ app/src/main/java/com/mambobryan/alcone/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mambobryan.alcone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LottieAnimationView mainAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            }
+
+
+}
